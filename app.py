f0e4cb6b5d78ae7935aebcbd3f5f6a50e2c4a4d0
nazwadiana38-lab <nazwadiana38@gmail.com>
2026-06-10 09:02:43 +0700

    [FEAT] Implementasi environment variables pada Dockerfile.

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import os
 
-# Membaca variabel 'APP_USER', defaultnya adalah 'Guest'
 user_name = os.getenv('APP_USER', 'Guest')
+app_env = os.getenv('APP_ENV', 'development')
 
 if __name__ == "__main__":
     print(f"Halo {user_name}! Aplikasi ini berjalan di dalam kontainer Docker.")
+    print(f"Status Lingkungan: {app_env}")
